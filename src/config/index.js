88a8c8df3efb49da1363054a1e09be1d57b31322
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-04-29 21:47:40 +0300

    up

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 const config = {
     production: {
         PORT: 1234,
-        DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        DB_URI: 'mongodb://127.0.0.1:27017/data',
         SECRET: 'SOMEPRODSECRET',
     },
     development: {
         PORT: 5000,
-        DB_URI: 'mongodb://127.0.0.1:27017/cubicle',
+        DB_URI: 'mongodb://127.0.0.1:27017/data',
         SECRET: 'SOMEDEVSECRET',
     }
 };
