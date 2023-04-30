b4f89dcd2592bfac6202a3f98739fd240315cb5f
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-04-30 14:05:52 +0300

    up

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,12 @@
 const config = {
     production: {
-        PORT: 8000, //https://server-node-pyu323d9s-zstoil.vercel.app
+        PORT: 7000, //https://server-node-pyu323d9s-zstoil.vercel.app
         DB_URI: 'mongodb+srv://stoilzlatin89:mG5jKz7gWp5sg8qP@data.xloathm.mongodb.net/?retryWrites=true&w=majority',
         SECRET: 'SOMEPRODSECRET',
     },
     development: {
         PORT: 5000,
-        DB_URI: 'mongodb://127.0.0.1:27017/data',
+        DB_URI: 'mongodb://127.0.0.1:27017/data',//mongodb://127.0.0.1:27017/data
         SECRET: 'SOMEDEVSECRET',
     }
 };
