ee97c2f09a0dfc7ee6abd17f97147761f7117df0
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-04-30 19:54:59 +0300

    up

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ const carLikeButton = require('./controllers/carLikeButton');
 const carUnLikeButton = require('./controllers/carUnLikeButton');
 const carComments = require('./controllers/carComments');
 
-router.use('/undefined/data/like',carLikeButton);
-router.use('/undefined/data/unLike',carUnLikeButton);
-router.use('/undefined/data/comments',carComments);
-router.use('/undefined/data/cars',carController);
-router.use('/undefined/users',authController);
+router.use('/data/like',carLikeButton);
+router.use('/data/unLike',carUnLikeButton);
+router.use('/data/comments',carComments);
+router.use('/data/cars',carController);
+router.use('/users',authController);
 
 module.exports = router;
