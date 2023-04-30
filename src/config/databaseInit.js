084fb7d339abfda05c3e5e24e59947cf79f021f1
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-04-30 19:11:15 +0300

    up

--- src/config/databaseInit.js
+++ src/config/databaseInit.js
@@ -5,7 +5,7 @@ const config = require('./index');
 async function initDatabase() {
     mongoose.set('strictQuery', false);
 
-    await mongoose.connect(config.DB_URI);
+    await mongoose.connect('mongodb+srv://stoilzlatin89:mG5jKz7gWp5sg8qP@data.xloathm.mongodb.net/?retryWrites=true&w=majority');//config.DB_URI
 
     console.log(`DB connected on ${config.DB_URI}`);
 }
