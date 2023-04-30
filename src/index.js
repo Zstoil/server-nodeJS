ed081585404a14ff74b118630c4c5d3c0d00831a
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-04-30 19:04:13 +0300

    up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const initDatabase = require('./config/databaseInit');
 const config = require('./config');
 
+
 const { authentication } = require ('../src/middlewares/authMiddleware');
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 app.use(authentication());
 app.use(routes);
+
 
 initDatabase()
     .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
