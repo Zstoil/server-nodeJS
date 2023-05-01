3ca7a8e68ba4753601366746c6843662edcc2e41
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 17:26:07 +0300

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     production: {
-        PORT: 8000,
+        PORT: 'server-node-b4gw4uyip-zstoil.vercel.app',
         DB_URI: 'mongodb+srv://stoilzlatin89:mG5jKz7gWp5sg8qP@data.xloathm.mongodb.net/?retryWrites=true&w=majority',
         SECRET: 'SOMEPRODSECRET',
     },
