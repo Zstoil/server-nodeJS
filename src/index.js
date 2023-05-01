c2c4068bbe786616d2e69543f31889387f1e81be
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 09:30:57 +0300

    up

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(authentication());
 app.use(routes);
 
-app.listen(process.env.DB_PORT);
+// app.listen(process.env.DB_PORT);
 
-// initDatabase()
-//     .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
-//     .catch((err) => console.error(err.message));
+initDatabase()
+    .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
+    .catch((err) => console.error(err.message));
 
 
 
