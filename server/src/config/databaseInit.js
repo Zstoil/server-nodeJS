dd4e698853508b27b3459af619757244263cf406
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-01 19:58:29 +0300

    test

--- server/src/config/databaseInit.js
+++ server/src/config/databaseInit.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const config = require('./index');
+// const config = require('./index');
 
-async function initDatabase() {
-    mongoose.set('strictQuery', false);
+// async function initDatabase() {
+//     mongoose.set('strictQuery', false);
 
-    await mongoose.connect(config.DB_URI);//config.DB_URI
+//     await mongoose.connect(process.env.DB_URI);//config.DB_URI
 
-    console.log(`DB connected on ${config.DB_URI}`);
-}
+//     console.log(`DB connected on ${config.DB_URI}`);
+// }
 
-module.exports = initDatabase;
+// module.exports = initDatabase;
--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
-const initDatabase = require('./config/databaseInit');
-const config = require('./config');
-
+// const initDatabase = require('./config/databaseInit');
+// const config = require('./config');
+const mongoose = require('mongoose');
 
 const { authentication } = require ('../src/middlewares/authMiddleware');
 
@@ -17,11 +17,18 @@ app.use(express.json());
 app.use(authentication());
 app.use(routes);
 
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.DB_URI);
+
+const port = process.env.DB_PORT || 3030
+
+app.listen(port)
 // app.listen(process.env.DB_PORT);
 
-initDatabase()
-    .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
-    .catch((err) => console.error(err.message));
+// initDatabase()
+//     .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`)))
+//     .catch((err) => console.error(err.message));
 
 
 
