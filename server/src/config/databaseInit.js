d65707522e31beb31a54950fd6bfb1d4a1cb6e3c
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-02 21:42:49 +0300

    Update databaseInit.js

--- server/src/config/databaseInit.js
+++ server/src/config/databaseInit.js
@@ -5,7 +5,7 @@ const config = require('./index');
 async function initDatabase() {
     mongoose.set('strictQuery', false);
 
-    await mongoose.connect(process.env.DB_URI);//config.DB_URI
+    await mongoose.connect(config.DB_URI);//process.env.DB_URI
 
     console.log(`DB connected on ${config.DB_URI}`);
 }
