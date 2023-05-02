299eedb68131ad85d6df123f59b0c79e307117a4
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-05-02 20:03:19 +0300

    test

--- server/src/services/carLikeService.js
+++
@@ -1,7 +0,0 @@
-const Like = require('../models/Like');
-
-exports.getAll = () => Like.find({});
-
-exports.create = (ownerId) => Like.create({_ownerId: ownerId});
-
-exports.delete = (id) => Like.findByIdAndDelete(id);
--- server/src/services/carUnLikeService.js
+++
@@ -1,7 +0,0 @@
-const Unlike = require('../models/UnLike');
-
-exports.getAll = () => Unlike.find({});
-
-exports.create = (ownerId) => Unlike.create({_ownerId: ownerId});
-
-exports.delete = (id) => Unlike.findByIdAndDelete(id);
